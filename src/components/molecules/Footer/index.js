b42a021cfd91a0f85f0230694399bff441658ddf
Bharat <majeti.bharat1@gmail.com>
2023-12-24 15:19:33 +0530

    sorting_visualizer

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        
+      </section>
+
+      <section className="Footer__Items">
+        
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
